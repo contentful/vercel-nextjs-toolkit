b0a7b411685cfc7387dbf7e27214e8f50be06c39
Matt Gordon <matt.gordon@contentful.com>
2024-04-23 13:44:11 -0600

    Remove watch flag config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    watch: false,
-  },
   build: {
     copyPublicDir: false,
     outDir: './dist',
