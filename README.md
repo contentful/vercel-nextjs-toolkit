80107a988a903b782eb5faf140e4e5f36c159421
Matt Gordon <matt.gordon@contentful.com>
2024-04-23 12:42:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+@contentful/vercel-toolkit
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    copyPublicDir: false,
+    outDir: './dist',
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+      fileName: 'main',
+    },
+    rollupOptions: {
+      external: [],
+    },
+  },
+  plugins: [
+    dts({
+      include: ['lib'],
+    }),
+  ],
+});
