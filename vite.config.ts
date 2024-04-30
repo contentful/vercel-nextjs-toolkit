25713cca147559adda80683316b675ef06a7270d
Matthew Gordon <matt.gordon@contentful.com>
2024-04-30 08:57:39 -0600

    fix: [] - Fix bundler config (#18)
    * fix: update compiler configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,23 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+import pkg from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    copyPublicDir: false,
-    outDir: './dist',
-    lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es'],
-      fileName: 'main',
-    },
-    rollupOptions: {
-      external: ['next'],
-    },
-  },
   plugins: [
     dts({
       include: ['lib'],
     }),
   ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+      fileName: () => 'main.js',
+    },
+    rollupOptions: {
+      external: [...Object.keys(pkg.peerDependencies)],
+    },
+  },
 });
