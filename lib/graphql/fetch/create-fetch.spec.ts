81a53f94bb0830c062fcdb6878ac7ae113670f2f
Matt Gordon <matt.gordon@contentful.com>
2024-05-02 12:16:16 -0600

    [WIP] update tests

--- lib/graphql/fetch/create-fetch.spec.ts
+++ lib/graphql/fetch/create-fetch.spec.ts
@@ -7,6 +7,14 @@ describe('createFetch', () => {
   const spaceId = 'testSpaceId';
   const accessToken = 'testAccessToken';
   const previewToken = 'testPreviewToken';
+  const graphqlEndpoint = `https://graphql.contentful.com/content/v1/spaces/${spaceId}`;
+
+  const { fetchGraphQL } = createFetch({
+    spaceId,
+    accessToken,
+    previewToken,
+  });
+
   const request = {
     query: `#graphql
       {
@@ -31,54 +39,40 @@ describe('createFetch', () => {
   });
 
   it('should call fetch with correct arguments for regular request', async () => {
-    const { fetchGraphQL } = createFetch({
-      spaceId,
-      accessToken,
-      previewToken,
-    });
+    const expectedResponse = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        query: request.query,
+        variables: request.variables,
+      }),
+      cache: 'force-cache',
+    };
 
     await fetchGraphQL(request.query, request.variables);
 
-    expect(fetch).toHaveBeenCalledWith(
-      `https://graphql.contentful.com/content/v1/spaces/${spaceId}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify({
-          query: request.query,
-          variables: request.variables,
-        }),
-        cache: 'force-cache',
-      },
-    );
+    expect(fetch).toHaveBeenCalledWith(graphqlEndpoint, expectedResponse);
   });
 
   it('should call fetch with correct arguments for preview request', async () => {
-    const { fetchGraphQL } = createFetch({
-      spaceId,
-      accessToken,
-      previewToken,
-    });
+    const expectedResponse = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${previewToken}`,
+      },
+      body: JSON.stringify({
+        query: request.query,
+        variables: request.variables,
+      }),
+      cache: 'no-store',
+    };
 
     await fetchGraphQL(request.query, request.variables, true);
 
-    expect(fetch).toHaveBeenCalledWith(
-      `https://graphql.contentful.com/content/v1/spaces/${spaceId}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${previewToken}`,
-        },
-        body: JSON.stringify({
-          query: request.query,
-          variables: request.variables,
-        }),
-        cache: 'no-store',
-      },
-    );
+    expect(fetch).toHaveBeenCalledWith(graphqlEndpoint, expectedResponse);
   });
 });
