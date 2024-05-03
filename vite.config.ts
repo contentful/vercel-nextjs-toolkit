0139112b37712c4e94381e92f9978980fc43650c
Jim Dalton <jim.dalton@gmail.com>
2024-05-03 08:45:05 -0700

    exclude spec files

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,9 @@ import pkg from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    dts({
-      include: ['lib'],
-    }),
-  ],
+  plugins: [dts({
+    exclude: ["lib/**/*.spec.ts"],
+  })],
   build: {
     ssr: true,
     minify: false,
