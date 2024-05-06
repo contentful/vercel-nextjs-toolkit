6d14a2c3a658351b117e78796a144ce965321260
Matt Gordon <matt.gordon@contentful.com>
2024-05-06 13:19:36 -0600

    [WIP] update exports

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
       entry: {
         'app-router': resolve(__dirname, 'lib/app-router/index.ts'),
         'pages-router': resolve(__dirname, 'lib/pages-router/index.ts'),
+        graphql: resolve(__dirname, 'lib/graphql/index.ts'),
       },
       formats: ['es', 'cjs'],
     },
